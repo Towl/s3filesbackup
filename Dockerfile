cecaff8b29860bf3a4cb4b8e0192ca9e3326ae1d
Franck <franck.coutouly@tms-software.ch>
2021-04-14 15:59:04 +0200

    Add save code mode

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,9 @@ FROM google/cloud-sdk:alpine
 
 RUN gcloud components install kubectl
 
-COPY backup.sh restore.sh install.sh /tmp/
+COPY backup.sh restore.sh install.sh save.sh /tmp/
 
-RUN chmod a+x /tmp/backup.sh /tmp/restore.sh /tmp/install.sh
+RUN chmod a+x /tmp/backup.sh /tmp/restore.sh /tmp/install.sh /tmp/save.sh
 
 VOLUME ["/data"]
 
---
+++ save.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "==> Identify pod to backup"
+echo "exec: kubectl get pod -l $BACKUP_POD_LABEL -o name"
+POD=$(kubectl get pod -l $BACKUP_POD_LABEL -o name)
+echo "selected pod: $POD"
+
+echo "==> Backup file from $POD:$PARENT_FOLDER/$RELATIVE_PATH"
+tar_opts=($TAR_OPTS)
+extract_cmd="kubectl exec $POD -- tar ${tar_opts[@]} -C $PARENT_FOLDER $RELATIVE_PATH -cf - $RELATIVE_PATH | tar xf - -C /data"
+echo "exec: $extract_cmd"
+eval $extract_cmd
+
+echo "==> Save current files to gs://$BUCKET/$PREFIX"
+echo "exec: gsutil cp $ARCHIVE_NAME.tar.gz gs://$BUCKET/$PREFIX"
+gsutil -m rsync -d -r /data gs://$BUCKET/$PREFIX
+
+echo "==> Done"
