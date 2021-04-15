5986f777a4c075cb71c0eee436b9e78cd469b727
Franck <franck.coutouly@tms-software.ch>
2021-04-15 16:43:53 +0200

    Add mode changing

--- install-archive.sh
+++ install-archive.sh
@@ -9,5 +9,7 @@ tar xzf $ARCHIVE_NAME.tar.gz && rm $ARCHIVE_NAME.tar.gz
 
 echo "==> Setup owner to $SEC_USER:$SEC_GROUP"
 chown -R $SEC_USER:$SEC_GROUP .
+find . -type d -exec chmod $SEC_FOLDER_MODE {} \;
+find . -type f -exec chmod $SEC_FILE_MODE {} \;
 
 echo "==> Done"
